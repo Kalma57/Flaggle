c3a21e8cd14454a40597508950af0a26f4857f09
Your Name <idankal@post.bgu.ac.il>
2026-03-05 11:23:06 +0200

    adding small changes to DAL

--- src/main/java/com/example/flagdemo/DataAccessLayer/CountryDAL.java
+++ src/main/java/com/example/flagdemo/DataAccessLayer/CountryDAL.java
@@ -5,17 +5,28 @@ public class CountryDAL {
     private String countryName;
     private String code;
     private String flagPath;
+    private double latitude;
+    private double longitude;
+    private String neighborsList;
 
-    public CountryDAL(int ID, String countryName, String code, String flagPath){
+    public CountryDAL(int ID, String countryName, String code, String flagPath, double lat, double longitude, String neigbors){
         this.ID = ID;
         this.countryName = countryName;
         this. code = code;
         this.flagPath = flagPath;
+        this.latitude = lat;
+        this.longitude = longitude;
+        this.neighborsList = neigbors;
+
     }
 
     public int getID() { return ID; }
     public String getCountryName() { return countryName; }
     public String getCode() { return code; }
     public String getFlagPath() { return flagPath; }
+    public double getLatitude() { return latitude; }
+    public double getLongitude() { return longitude; }
+    public String getNeighborsList() { return neighborsList; }
+
 
 }
